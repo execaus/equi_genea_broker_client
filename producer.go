29a829009fa248488cff8fc26da26fcb51b24deb
execaus <execaus@gmail.com>
2025-10-24 19:46:58 +0500

    Обновляет сигнатуры функций

--- producer.go
+++ producer.go
@@ -17,7 +17,7 @@ const (
 	TopicAccountActivity = "account_activity_topic"
 )
 
-func NewProducer(host string, port int) (*Producer, error) {
+func NewProducer(host, port string) (*Producer, error) {
 	cfg := sarama.NewConfig()
 	cfg.Producer.Return.Successes = true
 	cfg.Producer.RequiredAcks = sarama.WaitForAll
